ccb459a6abb8a772133df46cc24bcbc8c10792c2
Dominic Alejandro <dxalejandro97@gmail.com>
2020-10-07 12:43:33 -0700

    Initial commit of AddingMachine.

---
+++ AddingMachineRepository/cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,56 @@
+/*
+ * Author: Dominic Alejandro
+ * Class: CSE 360
+ * Assignment 2
+ * Description: The AddingMachine file implements methods to add, 
+ * 				subtract, and display integers
+ */
+
+package cse360assignment02;
+
+
+public class AddingMachine {
+	  private int total;
+	  /**
+	   * AddingMachine constructor initializes total to 0
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**
+	   * getTotal returns the total value
+	   * @return 0
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * add() sums the value to the current total
+	   * @param value : The value to be added to total
+	   */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * subtract() subtracts the value from the current total
+	   * @param value : The value to be subtracted from the total
+	   */
+	  public void subtract (int value) {
+	  }
+	  
+	  /**
+	   * toString() method
+	   * 
+	   * @return an empty string
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  /**
+	   * clear() clears the operation history and resets the 
+	   * total value back to 0
+	   */
+	  public void clear() {
+	  }
+}

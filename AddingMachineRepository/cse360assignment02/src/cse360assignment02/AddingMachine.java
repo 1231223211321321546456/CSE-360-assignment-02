9dd04acde40fbf60e5c610fd718d4178a82257d1
Dominic Alejandro <dxalejandro97@gmail.com>
2020-10-07 14:45:04 -0700

    Second commit

--- AddingMachineRepository/cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ AddingMachineRepository/cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -11,46 +11,70 @@ package cse360assignment02;
 
 public class AddingMachine {
 	  private int total;
+	  private String history;
 	  /**
 	   * AddingMachine constructor initializes total to 0
+	   * and history to "0"
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  /**
 	   * getTotal returns the total value
-	   * @return 0
+	   * @return total
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  /**
-	   * add() sums the value to the current total
+	   * add() sums the value to the current total and
+	   * records the added value in history
 	   * @param value : The value to be added to total
 	   */
 	  public void add (int value) {
+		  total += value;
+		  history += (" + " + value);
 	  }
 	  
 	  /**
-	   * subtract() subtracts the value from the current total
+	   * subtract() subtracts the value from the current total and
+	   * records the subtracted value in history
 	   * @param value : The value to be subtracted from the total
 	   */
 	  public void subtract (int value) {
+		  total -= value;
+		  history += (" - " + value);
 	  }
 	  
 	  /**
 	   * toString() method
 	   * 
-	   * @return an empty string
+	   * @return the string of the original value and the operations
+	   * performed on the value 
 	   */
 	  public String toString () {
-	    return "";
+		System.out.println(history);  
+	    return history;
 	  }
 	  /**
 	   * clear() clears the operation history and resets the 
-	   * total value back to 0
+	   * total value and history back to their starting values (0 and "0")
 	   */
 	  public void clear() {
+		  total = 0;
+		  history = "0";
+	  }
+	  public static void main(String args[]) {
+		  /*
+		  AddingMachine a = new AddingMachine();
+		  a.add(4);
+		  a.subtract(2);
+		  a.add(5);		  
+		  a.toString();
+		  */
 	  }
 }
+
+
